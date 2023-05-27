0274dfaf155978c6633038718c2dfb047605b962
2thlesss <thomas.jadie.reeves@gmail.com>
2023-05-27 12:32:59 -0400

    Initial Commit

---
+++ Sketchy Dungeons/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Sketchy Dungeons
+//
+//  Created by Thomas Jadie Reeves on 5/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sketchy Dungeons/Sketchy_DungeonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sketchy_DungeonsApp.swift
+//  Sketchy Dungeons
+//
+//  Created by Thomas Jadie Reeves on 5/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Sketchy_DungeonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
